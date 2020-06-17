814114d04479bd1cce605a1e7dd266af859188d6
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2020-06-17 08:22:26 +0300

    user reg integrated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,14 +22,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    // public function boot(UrlGenerator $url)
-    // {
+
     public function boot(UrlGenerator $url)
     {
 
 
-        // if (env('APP_ENV') !== 'local') {
-        //     $url->forceScheme('https');
-        // }
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        }
     }
 }
